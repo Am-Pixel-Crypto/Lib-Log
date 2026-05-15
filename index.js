fdab9245d2ef8f80369a512e42724c7c8f39a549
Am-Pixel-Crypto <mahmoudmostafa12500@gmail.com>
2026-05-15 17:28:58 +0300

    added env.port for hosting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const db = new pg.Client({
   user: process.env.DB_USER,
